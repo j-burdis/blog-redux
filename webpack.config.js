aee992772ba60a2b9e4aff34c9f2aeba9a245f2d
Jonathan Burdis <jonnyburdis@hotmail.com>
2024-12-23 18:51:37 +0000

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'eval-source-map',
+  devServer: { historyApiFallback: true },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.s?css$/,
+        exclude: /node_modules/,
+        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.js', '.jsx' ]
+  }
+};
